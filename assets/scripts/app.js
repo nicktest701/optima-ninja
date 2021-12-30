00b5fc1d07b0a1c80ebf3cca0d35718c29f91353
unknown <nicktest701@gmail.com>
2021-12-30 13:15:59 +0000

    make changes to css file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,29 @@
-// alert("Hello");
+let menu = document.querySelector(".menu");
+let navbar = document.querySelector(".nav-bar");
+let navItems = document.querySelectorAll(".nav-item-link");
+let scrollToTop = document.querySelector(".scroll-to-top");
+
+menu.addEventListener("click", (e) => {
+  navbar.classList.toggle("close");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 768) {
+    navbar.classList.remove("close");
+  }
+});
+
+window.addEventListener("scroll", (e) => {
+  // 1170
+  //5100
+  window.scrollY < 1170
+    ? (scrollToTop.style.display = "none")
+    : (scrollToTop.style.display = "grid");
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", (e) => {
+    console.log("hello");
+    navbar.classList.remove("close");
+  });
+});
